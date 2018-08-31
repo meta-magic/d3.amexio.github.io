51af2e53b73a8c55d94ee0ae1403eae23fd2a0b5
Manisha1205 <manisha.boddu@metamagic.in>
2018-08-31 15:12:55 +0530

    barchart with tooltip

--- projects/amexio-chart-d3/src/lib/bar/bar.component.ts
+++ projects/amexio-chart-d3/src/lib/bar/bar.component.ts
@@ -20,32 +20,42 @@ export class AmexioD3BarChartComponent {
   description : provides data to chart
   */
   @Input('data') dataset: any;
- /*
-  Properties
-  name : color
-  datatype : string
-  version : 5.2 onwards
-  default : none
-  description : set color to chart
-  */
+  /*
+   Properties
+   name : color
+   datatype : string
+   version : 5.2 onwards
+   default : none
+   description : set color to chart
+   */
   @Input('color') colors: string[];
-/*
-  Properties
-  name : width
-  datatype : any
-  version : 5.2 onwards
-  default : none
-  description : set width to chart
-  */
+  /*
+    Properties
+    name : width
+    datatype : any
+    version : 5.2 onwards
+    default : none
+    description : set width to chart
+    */
   @Input('width') svgwidth: any;
-/*
-  Properties
-  name : height
-  datatype : any
-  version : 5.2 onwards
-  default : none
-  description : set height to chart
-  */
+  /*
+    Properties
+    name : width
+    datatype : any
+    version : 5.2 onwards
+    default : none
+    description : set width to chart
+    */
+  @Input() tooltip: boolean = true;
+
+  /*
+    Properties
+    name : height
+    datatype : any
+    version : 5.2 onwards
+    default : none
+    description : set height to chart
+    */
   @Input('height') svgheight: any;
   /*
   Properties
@@ -56,7 +66,7 @@ export class AmexioD3BarChartComponent {
   description : set title to chart
   */
   @Input() title: any = "";
- 
+
   colorforhtml = [];
   predefinedcolors = [];
 
@@ -72,7 +82,7 @@ export class AmexioD3BarChartComponent {
     }, 0);
 
   }
-                      
+
   plotChart() {
     let color;
     let i;
@@ -80,6 +90,9 @@ export class AmexioD3BarChartComponent {
     let top = 70;
     let labels = [];
     let legendcolors = color;
+    const tooltip = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0);
     //defining svg
     let svg = d3.select("svg"),
       margin = { top: 20, right: 20, bottom: 30, left: 40 },
@@ -109,26 +122,19 @@ export class AmexioD3BarChartComponent {
       .attr("height", function (d) { return height - y(d.value); })
       .style("fill", function (d) {
         return d.color;
-      });
+      })
+      .on('mouseover', function (d) {
+          tooltip.transition().duration(100).style('opacity', 1);
+          tooltip.html(`Value: <span>${d.value}</span>`)
+            .style("left", d3.event.pageX + "px")
+            .style("top", d3.event.pageY + "px")
+      })
+      .on('mouseout', () => tooltip.transition().duration(500).style('opacity', 0));;
     this.dataset.forEach(element => {
       if (element.label) {
         labels.push(element.label);
       }
     });
-    labels.forEach(element => {
-      this.labelstack.push(element);
-    });
-    this.legendArray = [];
-    for (i = 0; i < this.labelstack.length; i++) {
-      let obj = {};
-      obj['label'] = this.labelstack[i];
-      if (this.colorforhtml[i]) {
-        obj['color'] = this.colorforhtml[i];
-      }
-      else
-        obj['color'] = 'black';
-      this.legendArray.push(obj);
-    }
   }
 
   getColors(object: any) {
