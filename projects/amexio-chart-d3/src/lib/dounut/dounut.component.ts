fbf202b8199150f7232f31ec54f49afe21c0218e
Manisha1205 <manisha.boddu@metamagic.in>
2018-09-01 20:57:18 +0530

    pie donut with legend as attr

--- projects/amexio-chart-d3/src/lib/dounut/dounut.component.ts
+++ projects/amexio-chart-d3/src/lib/dounut/dounut.component.ts
@@ -9,12 +9,27 @@ export class AmexioD3DounutChartComponent
 {
   predefinedcolors: string[];
   colorIndex: number = 0;
+  labelstack = [];
+  legendArray = [];
+  colorforhtml = [];
+
     constructor() {
       this.predefinedcolors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
     }
  
     @Input('data') dataset: any;
  
+
+    /*
+  Properties
+  name : legend
+  datatype : boolean
+  version : 5.2 onwards
+  default : true
+  description : set legend
+  */
+ @Input() legend: boolean = true ;
+
     @Input('color') colors: any;
     
     @Input('width') svgwidth: any;
@@ -35,7 +50,8 @@ export class AmexioD3DounutChartComponent
    plotChart(){
     let flag =3;
     let color;
-  //this.initializeData();
+
+  this.initializeData();
     const tooltip = d3.select('body').append('div')
     .attr('class', 'tooltip')
     .style('opacity', 0);
@@ -66,18 +82,57 @@ switch(flag){
       colorarray1.push(this.predefinedcolors[i]);
     }
   });
+  this.colorforhtml =[];
+  colorarray1.forEach(element => {
+    this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(colorarray1);
   break;
 
   case 2:
+  this.colorforhtml =[]
+  this.colors.forEach(element => {
+  this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(this.colors);
   break;
 
   case 3:
+  this.colorforhtml =[]
+  this.predefinedcolors.forEach(element => {
+  this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(this.predefinedcolors);
   break;
 }
  
+
+//logic for legend
+
+var labelsforlegend = [];
+// var legendcolors = color;
+ this.dataset.forEach(element => {
+   if (element.label) {
+     labelsforlegend.push(element.label);
+   }
+ });
+ labelsforlegend.forEach(element => {
+   this.labelstack.push(element);
+ });
+ this.legendArray = [];
+ 
+ for (let i = 0; i < this.labelstack.length; i++) {
+   let obj = {};
+   obj['label'] = this.labelstack[i];
+   if (this.colorforhtml[i]) {
+     obj['color'] = this.colorforhtml[i];
+   }
+   else
+     obj['color'] = 'black';
+   this.legendArray.push(obj);
+ }
+
+
     let svgWidth;
  
     let svgHeight;
@@ -109,6 +164,14 @@ let top = 0;
      }
    });                   
   
+
+
+
+
+
+
+
+   
      let outerRadius=svgWidth/2;
 
     let innerRadius=svgWidth/4;
@@ -174,8 +237,7 @@ let text=svg.selectAll('text')
 
 
 generateRandomText(){
-  debugger;
-  let text = "";
+   let text = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcddefghijklmonpqrstuvwxyz";
   for(let i = 0; i<5; i++){
     text = text + possible.charAt(Math.floor(Math.random()*possible.length));
@@ -184,33 +246,38 @@ generateRandomText(){
   return text;
 }
 
-  //  getColors(object: any) {
-  //   if (object.color) {
-  //      return object.color;
-  //   }
-  //   else if (this.colors.length > this.colorIndex) {
-  //     const color = this.colors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }
-  //   else if ((this.colors.length > 0) && (this.colors.length <= this.colorIndex)) {
-  //     this.colorIndex = 0;
-  //     const color = this.colors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }
-  //   else {
-  //     const color = this.predefinedcolors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }
-  // }
+   getColors(object: any) {
+    if (object.color) {
+      this.colorforhtml.push(object.color);
+       return object.color;
+    }
+    else if (this.colors.length > this.colorIndex) {
+      const color = this.colors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+    else if ((this.colors.length > 0) && (this.colors.length <= this.colorIndex)) {
+      this.colorIndex = 0;
+      const color = this.colors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+    else {
+      const color = this.predefinedcolors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+  }
 
-  // initializeData() {
-  //   this.dataset.forEach(element => {
-  //     element.color = this.getColors(element);
-  //   });
-  // }
+  initializeData() {
+    this.dataset.forEach(element => {
+      element.color = this.getColors(element);
+      console.log("element.color", element.color);
+    });
+  }
 
 
 }
--- projects/amexio-chart-d3/src/lib/pie/pie.component.ts
+++ projects/amexio-chart-d3/src/lib/pie/pie.component.ts
@@ -6,14 +6,30 @@ import * as d3 from 'd3';
   styleUrls: ['./pie.component.css']
 })
 export class PieComponent implements OnInit {
+  
   predefinedcolors: string[];
   colorIndex: number = 0;
+  labelstack = [];
+  legendArray = [];
+  colorforhtml = [];
+
     constructor() {
       this.predefinedcolors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
     }
  
     @Input('data') dataset: any;
  
+
+    /*
+  Properties
+  name : legend
+  datatype : boolean
+  version : 5.2 onwards
+  default : true
+  description : set legend
+  */
+ @Input() legend: boolean = true ;
+
     @Input('color') colors: any;
     
     @Input('width') svgwidth: any;
@@ -30,11 +46,12 @@ export class PieComponent implements OnInit {
       this.plotChart();
     }, 0);
    }
-
+                                                  
    plotChart(){
     let flag =3;
     let color;
-  // this.initializeData();
+
+  this.initializeData();
     const tooltip = d3.select('body').append('div')
     .attr('class', 'tooltip')
     .style('opacity', 0);
@@ -65,18 +82,57 @@ switch(flag){
       colorarray1.push(this.predefinedcolors[i]);
     }
   });
+  this.colorforhtml =[];
+  colorarray1.forEach(element => {
+    this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(colorarray1);
   break;
 
   case 2:
+  this.colorforhtml =[]
+  this.colors.forEach(element => {
+  this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(this.colors);
   break;
 
   case 3:
+  this.colorforhtml =[]
+  this.predefinedcolors.forEach(element => {
+  this.colorforhtml.push(element);
+  });
   color = d3.scaleOrdinal(this.predefinedcolors);
   break;
 }
  
+
+//logic for legend
+
+var labelsforlegend = [];
+// var legendcolors = color;
+ this.dataset.forEach(element => {
+   if (element.label) {
+     labelsforlegend.push(element.label);
+   }
+ });
+ labelsforlegend.forEach(element => {
+   this.labelstack.push(element);
+ });
+ this.legendArray = [];
+ 
+ for (let i = 0; i < this.labelstack.length; i++) {
+   let obj = {};
+   obj['label'] = this.labelstack[i];
+   if (this.colorforhtml[i]) {
+     obj['color'] = this.colorforhtml[i];
+   }
+   else
+     obj['color'] = 'black';
+   this.legendArray.push(obj);
+ }
+
+
     let svgWidth;
  
     let svgHeight;
@@ -106,8 +162,16 @@ let top = 0;
      if (element.label) {
        VALUES.push(element.label);
      }
-   });
+   });                   
   
+
+
+
+
+
+
+
+   
      let outerRadius=svgWidth/2;
 
     let innerRadius=0;
@@ -173,8 +237,7 @@ let text=svg.selectAll('text')
 
 
 generateRandomText(){
-  debugger;
-  let text = "";
+   let text = "";
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcddefghijklmonpqrstuvwxyz";
   for(let i = 0; i<5; i++){
     text = text + possible.charAt(Math.floor(Math.random()*possible.length));
@@ -183,34 +246,39 @@ generateRandomText(){
   return text;
 }
 
-  //  getColors(object: any) {
-  //   if (object.color) {
-  //      return object.color;
-  //   }
-  //   else if (this.colors.length > this.colorIndex) {
-  //     const color = this.colors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }
-  //   else if ((this.colors.length > 0) && (this.colors.length <= this.colorIndex)) {
-  //     this.colorIndex = 0;
-  //     const color = this.colors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }                                              
-                              
-  //   else {
-  //     const color = this.predefinedcolors[this.colorIndex];
-  //     this.colorIndex++;
-  //      return color;
-  //   }
-  // }
+   getColors(object: any) {
+    if (object.color) {
+      this.colorforhtml.push(object.color);
+       return object.color;
+    }
+    else if (this.colors.length > this.colorIndex) {
+      const color = this.colors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+    else if ((this.colors.length > 0) && (this.colors.length <= this.colorIndex)) {
+      this.colorIndex = 0;
+      const color = this.colors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+    else {
+      const color = this.predefinedcolors[this.colorIndex];
+      this.colorIndex++;
+      this.colorforhtml.push(object.color);
+       return color;
+    }
+  }
 
-  // initializeData() {
-  //   this.dataset.forEach(element => {
-  //     element.color = this.getColors(element);
-  //   });
-  // }
-                               
+  initializeData() {
+    this.dataset.forEach(element => {
+      element.color = this.getColors(element);
+      console.log("element.color", element.color);
+    });
+  }
+
+
 
 }
