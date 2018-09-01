e7b3a885f2053b859a12b08ccd312b26b2d1dec1
kedar8 <kedar.kokil@metamagic.in>
2018-09-01 23:42:03 +0530

    data: commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,81 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  donutData = [
-    { label: "IE", value: 39.10 },
-    { label: "Chrome", value: 32.51 },
-    { label: "Safari", value: 13.68 },
-    { label: "Firefox", value: 8.71 },
-    { label: "Others", value: 6.01 }
-];
+    title = 'app';
+    donutData = [
+        { label: "IE", value: 39.10 },
+        { label: "Chrome", value: 32.51 },
+        { label: "Safari", value: 13.68 },
+        { label: "Firefox", value: 8.71 },
+        { label: "Others", value: 6.01 }
+    ];
 
-barchartcolor = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
+    barchartcolor = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
 
-barchartdata = [
-  {
-      "label": "A",
-      "value": 180,
-      "color": "blue"
-  },
-  {
-      "label": "B",
-      "value": 100,
-      "color": "yellow"
-  },
-  {
-      "label": "C",
-      "value": 56,
-      "color": "green"
-  },
-  {
-      "label": "D",
-      "value": 120,
-      "color": "blue"
-  },
-  {
-      "label": "E",
-      "value": 90,
-      "color": "yellow"
-  },
-  {
-      "label": "F",
-      "value": 140,
-      "color": "green"
-  },
-  {
-      "label": "G",
-      "value": 130,
-      "color": "blue"
-  },
-  {
-      "label": "H",
-      "value": 110,
-      "color": "yellow"
-  },
-  {
-      "label": "I",
-      "value": 70,
-      "color": "green"
-  },
-  {
-      "label": "J",
-      "value": 170,
-      "color": "blue"
-  }
-];
-
-  testLegend(event: any){
-    debugger;
-
-  }
-
+    barchartdata = [
+        {
+          "label": "Card",
+          "value": "90",
+          "children": [
+            {
+              "label": "Form",
+              "value": "40"
+            },
+            {
+              "label": "Image",
+              "value": "09"
+            }
+          ]
+        },
+        {
+          "label": "B",
+          "value": 100,
+          "color": "yellow"
+        },
+        {
+          "label": "C",
+          "value": 56,
+          "color": "green"
+        },
+        {
+          "label": "D",
+          "value": 120,
+          "color": "blue"
+        },
+        {
+          "label": "E",
+          "value": 90,
+          "color": "yellow"
+        },
+        {
+          "label": "F",
+          "value": 140,
+          "color": "green"
+        },
+        {
+          "label": "G",
+          "value": 130,
+          "color": "blue"
+        },
+        {
+          "label": "H",
+          "value": 110,
+          "color": "yellow"
+        },
+        {
+          "label": "I",
+          "value": 70,
+          "color": "green"
+        },
+        {
+          "label": "J",
+          "value": 170,
+          "color": "blue"
+        }
+      ];
 }
