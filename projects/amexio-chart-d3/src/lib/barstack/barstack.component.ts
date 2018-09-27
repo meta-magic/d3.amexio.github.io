177e73107f1b201f90886ecf855532c881d8341d
Manisha1205 <manisha.boddu@metamagic.in>
2018-09-27 18:50:52 +0530

    barstack updated

--- projects/amexio-chart-d3/src/lib/barstack/barstack.component.ts
+++ projects/amexio-chart-d3/src/lib/barstack/barstack.component.ts
@@ -17,12 +17,14 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
   legends: any[];
   charttype: string;
   data: any[];
+  datareaderdata: any[];
   @Input('data') data1: any
   @Input() barwidth: number = 0;
   @Input() title: String = "";
   @Input() legend: boolean = true;
   @Input() color: string[] = [];
   @Input('width') svgwidth: number = 300;
+  @Input('data-reader') datareader: any;
   @Input('height') svgheight: number = 300;
   @ViewChild('chartId') chartId: ElementRef;
   @Output() onLegendClick: any = new EventEmitter<any>();
@@ -36,14 +38,15 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
     // setTimeout(() => {
     //   this.plotChart();
     // }, 0);
-
+    let res;
     if (this.httpmethod && this.httpurl) {
       this.myservice.fetchData(this.httpurl, this.httpmethod).subscribe((response) => {
-        this.data = response;
+        //this.data = response;
+        this.data = this.getResponseData(response);
       }, (error) => {
       }, () => {
         setTimeout(() => {
-          // this.transformData(this.data1);
+          this.transformData(this.data);
           this.plotChart();
         }, 0);
       });
@@ -59,7 +62,7 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
   transformData(data1: any) {
     this.keyArray = [];
     this.legendArray = [];
-    this.data1.forEach((element, i) => {
+    data1.forEach((element, i) => {
       if (i == 0) {
         element.forEach((innerelement, index) => {
           if (index > 0) {
@@ -120,23 +123,21 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
       this.keyArray = keysetarray;
       this.keyArray.splice(0, 1);
     }
-    let series = d3.stack()
-      .keys(
-        this.keyArray
-      )
+    let series = d3.stack().keys(this.keyArray)
       .offset(d3.stackOffsetDiverging)
       (this.data);
-
+    series
     let svg = d3.select("#" + this.componentId),
-      width = +this.svgwidth - margin.left - margin.right,
-      height = +svg.attr("height");
+      width = this.svgwidth - margin.left - margin.right;
+
+    let height = +svg.attr("height");
 
     let x = d3.scaleBand()
       .domain(data.map((d) => {
         return d[Object.keys(d)[0]];
       }))
       .rangeRound([margin.left, width - margin.right])
-      .padding(0.1);
+       .padding(0.1);
 
     let y = d3.scaleLinear()
       .domain([d3.min(this.stackMin(series)), d3.max(this.stackMax(series))])
@@ -175,9 +176,13 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
       .attr("x", (d) => {
         return x(+d.data[Object.keys(d.data)[0]]);
       })
-      .attr("y", (d) => { return y(d[1]); })
+      .attr("y", (d, index) => {
+        return y(d[1]);
+      })
       .attr("cursor", "pointer")
-      .attr("height", (d) => { return y(d[0]) - y(d[1]); })
+      .attr("height", (d, index) => {
+        return y(d[0]) - y(d[1]);
+      })
       .on("mouseover", (d) => {
         return tooltip.style("visibility", "visible");
       })
@@ -232,7 +237,7 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
       }
     }
   }
-   setBarClickText(d: any) {
+  setBarClickText(d: any) {
     let diff = d[0] - d[1];
     if (diff < 0) {
       diff = (diff * (-1));
@@ -245,9 +250,19 @@ export class BarstackComponent extends AmexioD3BaseChartComponent implements OnI
       }
     }
   }
-  // barStackChartClick(d: any){
-  //   let obj = {'label': }
-  //   this.chartClick(d);
 
-  // }
+  getResponseData(httpResponse: any) {
+    let responsedata = httpResponse;
+    if (this.datareader != null) {
+      const dr = this.datareader.split('.');
+      for (const ir of dr) {
+        responsedata = responsedata[ir];
+      }
+    } else {
+      responsedata = httpResponse;
+    }
+
+    return responsedata;
+  }
+
 }
